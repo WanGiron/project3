0c247a14649ba4512e8fa2f6088e16256cf12ad7
William and Lady Navas <ladysantoyo@Williams-MacBook-Pro.local>
2019-05-13 17:30:27 -0400

    refresh

--- server.js
+++ server.js
@@ -255,11 +255,13 @@ app.get("/api/db/favItems/users", (req, res) => {
 var PORT = process.env.PORT || 5005;
 // Only for Deployment -HEROKU- Serve up static assets DO NOT TOUCH !!!
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));    
+    app.use(express.static("client/build"));  
+    
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    })  
 };
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-})
+
 
 // Handles any requests that don't match the ones above
 // app.get('*', (req, res) => {
