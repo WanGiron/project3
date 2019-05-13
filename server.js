2a114a069aa9b86c73922e1f585b7b66c658c73b
William and Lady Navas <ladysantoyo@williams-mbp.uh.uhnj.org>
2019-05-13 14:25:18 -0400

    reresh

--- server.js
+++ server.js
@@ -20,12 +20,14 @@ var PORT = process.env.PORT || 5005;
 // Only for Deployment -HEROKU- Serve up static assets DO NOT TOUCHE !!!
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+    })
     
 };
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-})
+
 
 
 // Serve the static files from the React app
