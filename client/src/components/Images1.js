754c038141230d538e8673df237b767ea83f3c57
William and Lady Navas <ladysantoyo@williams-mbp.uh.uhnj.org>
2019-05-13 14:05:18 -0400

    refresh

--- client/src/components/Images1.js
+++ client/src/components/Images1.js
@@ -6,7 +6,7 @@ function Images(props){
    <div className='card div-image'>
         <div className="container fav-images">
             <img src={props.url} className='photos user-photo' alt="pics"></img>
-            <button  className="button1" onClick={()=> props.handleClickDelete(props.url)}><a href="#" className="button-x">X</a></button>
+            <button  className="button1" onClick={()=> props.handleClickDelete(props.url)}><a href="" className="button-x">X</a></button>
         </div>
     </div>
        )
--- client/src/components/Images2.js
+++ client/src/components/Images2.js
@@ -6,7 +6,7 @@ function Images(props){
   return <div className="card">
       <div className='div-image'>
       <img src={props.url} className='photos' alt="pics"></img>
-      <button  className="button1" onClick={()=> props.handleClick(props.url)}><a href="#" className="button-x">&#x2764;</a></button>
+      <button  className="button1" onClick={()=> props.handleClick(props.url)}><a href="" className="button-x">&#x2764;</a></button>
        
       </div>
       </div>
